34658f895909e7917c433605d510a8a457fdfaef
paprika-flow <clementjoseph28@gmail.com>
2025-10-04 01:03:30 -0400

    modified ros2_humble.sh to mount workspace file on container

---
+++ modified_ros2_humble.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+xhost +
+docker run -it \
+--privileged=true \
+--net=host \
+--env="DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+-v /tem/.X11-unix:/tmp/.X11-unix \
+--security-opt apparmor:unconfined \
+-v /dev/input:/dev/input \
+-v /dev/video0:/dev/video0 \
+-v /home/pi/workspaces:/root/workspaces \
+yahboomtechnology/ros-humble:4.1.2 /bin/bash /root/1.sh 
+
