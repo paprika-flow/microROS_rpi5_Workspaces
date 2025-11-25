62ceadeb4c7cc6480e05accd94687fd4f224cab6
LucasBianco <130702524+LucasBianco@users.noreply.github.com>
2025-11-25 21:11:49 +0000

    Update PID parameters and add left control logic
    Adjusted PID constants and modified function behavior based on new parameters.

--- EdgeLeftFollowing/PID.py
+++ EdgeLeftFollowing/PID.py
@@ -3,11 +3,12 @@ import math
 # defining constants for PID to go forward
 Kp = 0.5
 Ki = 0.005
-Kd = 0.7
+Kd = 0.75
 
 
 
 def function_modifier(function, limit):
+    function = function/2
     # a function that limit another function regarding limits given
     if function > limit:
         return limit
@@ -27,14 +28,14 @@ def D_sum_list(list, t):
     derivative = list[t] - list[t-1]
     return derivative
 
-def PID_sidewalk(slope, intercept, error_list):
+def PID_sidewalk(slope, intercept, error_list, left):
     error_intercept = 0
     error_slope = 0
     # since the difference of the slope is slower than the intercept, I will divide by a factor
     factor = 100
-    print(f"slope = {slope}")
-    print(f"intercept = {intercept}")
-    print(f'{len(error_list)}')
+    #print(f"slope = {slope}")
+    #print(f"intercept = {intercept}")
+    #print(f'{len(error_list)}')
     if intercept > 405:
         error_intercept = (intercept - 405)/ factor
     elif intercept < 375:
@@ -52,6 +53,8 @@ def PID_sidewalk(slope, intercept, error_list):
     t = len(error_list) - 1 # find what point in position t in the list the robot is now7
     # mulitplying by -1, because positive twist.angular.z turns right
     function = -1 * (Kp * error_list[t] + Ki * I_sum_list(error_list) + Kd * D_sum_list(error_list, t))
-    angularz = function_modifier(function, 2)
+    angularz = function_modifier(function, 0.2)
+    if not left:
+        angularz *= -1
     print(angularz)
     return angularz , error_list
